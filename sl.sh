ca55a1b069055754c4dd6bfca5ae17dfc57466d9
Vokda <djing_djang@hotmail.com>
2016-08-10 15:39:21 +0200

    Update sl.sh

--- sl.sh
+++ sl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#is meant to be used together with i3wm and dmenu
 #required software: i3wm, dmenu, sl, xdotool
-#train goes from right to left start it from the right most screen.
+#train goes from right to left, start it from the right most screen.
 
 sl& 
 xdotool mousemove_relative -- -2000 0 & xdotool key Super+d ; xdotool type i3-sensible-terminal ; xdotool key Return
